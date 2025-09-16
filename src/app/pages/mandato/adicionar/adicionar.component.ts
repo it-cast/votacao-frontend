249e744e18b92165a2cb4d743fbc5fbb48900ec8
Carlos6464 <adrianosodre23@gmail.com>
2025-09-16 16:30:41 -0300

    feat: iniciando a tela de mandato

---
+++ src/app/pages/mandato/adicionar/adicionar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adicionar',
+  imports: [],
+  templateUrl: './adicionar.component.html',
+  styleUrl: './adicionar.component.scss'
+})
+export class AdicionarComponent {
+
+}
---
+++ src/app/pages/mandato/mandato.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mandato',
+  imports: [],
+  templateUrl: './mandato.component.html',
+  styleUrl: './mandato.component.scss'
+})
+export class MandatoComponent {
+
+}
